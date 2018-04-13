f29d413ffa68ce636ff6035171e360c4e2e0073f
lborges123 <luis.borges123@hotmail.com>
2018-04-13 15:17:39 +0100

    stuff done

--- simgrep/src/main.c
+++ simgrep/src/main.c
@@ -8,7 +8,20 @@
 #include <sys/stat.h>
 #include <dirent.h>
 
+typedef struct {
+	int i;
+	int l;
+	int n;
+	int c;
+	int w;
+	int r;
+} options;
+
 int main(int argc, char** argv) {
+
+	char optionsUsed[5];
+
+	options opt = { .i = 0, .l = 0, .n = 0, .c = 0, .w = 0, .r = 0 };
 
 	/* check for invalid command inputs */
 	int invalidInputs = check_inputs(argc, argv);
@@ -26,11 +39,35 @@ int main(int argc, char** argv) {
 	/* start process forking here */
 
 	/* search for pattern */
-	if (argc == 2) {
-		search_pattern(argv[1], "test");
-	} else {
-		search_pattern(argv[2], "test");
+
+	for (int i = 1; i < argc; i++) {
+
+		if (strcmp(argv[i], '-i')) {
+			opt.i = 1;
+		}
+
+		if (strcmp(argv[i], '-l')) {
+			opt.l = 1;
+		}
+
+		if (strcmp(argv[i], '-n')) {
+			opt.n = 1;
+		}
+
+		if (strcmp(argv[i], '-c')) {
+			opt.c = 1;
+		}
+
+		if (strcmp(argv[i], '-w')) {
+			opt.w = 1;
+		}
+
+		if (strcmp(argv[i], '-r')) {
+			opt.r = 1;
+		}
 	}
+
+	search_pattern(argv[1], "test", &opt);
 
 	/* closes log file */
 	close_logFile();
--- simgrep/src/utils.c
+++ simgrep/src/utils.c
@@ -1,4 +1,5 @@
 #include "utils.h"
+#include "main.c"
 
 #define UNKNOWN -1
 
@@ -8,7 +9,7 @@ int fd_fileToSearch = UNKNOWN;
 
 int check_inputs(int argc, char** argv) {
 
-	char* options[] = { "-i","-l","-n","-c","-w","-r" };
+	char* options[] = { "-i", "-l", "-n", "-c", "-w", "-r" };
 
 	if (argc < 2 || argc > 4)
 		return 1;
@@ -72,10 +73,16 @@ int open_file(const char* filename) {
 	return 0;
 }
 
-void search_pattern(char* pattern, char* file) {
+void search_pattern(char* pattern, char* file, options *opt) {
 
-	char str[100];
+	char str[100]; //string com todo o ficheiro a procurar
+
+	int line = 1;
+	int startIndex; //indice do inicio de cada linha
 	int i = 0, k = 0, count = 0;
+	int whole = 1;
+	int foundWord = 0;
+	int indexWord;
 
 	/* open file to search */
 	open_file(file);
@@ -86,24 +93,67 @@ void search_pattern(char* pattern, char* file) {
 		/* add terminating char */
 		str[99] = '\0';
 
+		/* put chars in lower case */
+		if (opt->i) {
+			for (int j = 0; j < sizeof(str); j++) {
+				str[j] = tolower(str[j]);
+			}
+			for (int j = 0; j < sizeof(pattern); j++) {
+				pattern[j] = tolower(pattern[j]);
+			}
+		}
+
 		/* search for substring */
 		while (str[i] != '\0') {
 
-			if (str[i] == pattern[k]) k++;
-			else k = 0;
+			/*new line*/
+			if (str[i] == '\n') {
+				line++;
+				startIndex = i + 1;
+			}
 
-			/* pattern found */
-			if (k == strlen(pattern)) {
-				count++;
-				k = 0;
+			if (str[i] == pattern[k]) {
+				if (foundWord == 0) {  //guarda o indice do inicio da palavra
+					indexWord = i;
+				}
+				foundWord = 1;
+				k++;
+			} else {
+				foundWord = 0;
+			}
+			k = 0;
+		}
+		/* pattern found */
+		if (k == strlen(pattern)) {
+			count++;
+			k = 0;
+
+			if (opt->w) { /* verifies if is whole word*/
+				if ((str[i + 1] == '.' || str[i + 1] == ' ')
+						&& (str[indexWord - 1] == ' ' || str[indexWord - 1] == '\n'))
+					whole = 1;
+				whole = 0;
+			}
+
+			/* line number*/
+			if (opt->n && (whole == 1))
+				printf("%d :", line);
+
+			if ((opt->l == 0) && (opt->c == 0) && (whole == 1)) { //default mode
+				int index = startIndex;
+				while (str[index] != '.') {
+					printf("%c", str[index]);   //prints the sentence
+					index++;
+				}
 			}
 			i++;
 		}
-
 		i = 0;
 	}
 
-	printf("Number of patterns found = %d\n", count);
+	if (opt->c) { /* number of occurrences */
+		printf("%d\n", count); //varias vezes na mesma linha?
+	}
 
 	/* closing text file */
 	close(fd_fileToSearch);
